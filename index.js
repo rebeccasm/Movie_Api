e49c51f8f9f85bddb7d92a83634e85a111e3d643
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-09-09 16:45:59 +0200

    Update

--- index.js
+++ index.js
@@ -1,37 +1,214 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+const express = require('express'),
+      morgan = require('morgan'),
+      mongoose = require('mongoose'),
+      Models = require('./models.js');
+
+const Movies = Models.Movie,
+      Users = Models.User;
+
 
 const app = express();
 
-// Top 10 movies data
-const topMovies = [
-    { title: 'Ruby Red', director: 'Felix Fuchssteiner' },
-    { title: 'Vampire Academy', director: 'Mark Waters' },
-    { title: 'Pirates of the Caribbean', director: 'Gore Verbinski' },
-    { title: 'National Treasure', director: 'Jon Turteltaub' },
-    { title: 'Iron Man', director: 'Jon Favreau' },
-    { title: 'The Fast and the Furious', director: 'Rob Cohen' },
-    { title: 'Damsel', director: 'Juan Carlos Fresnadillo' },
-    { title: 'Avengers: Endgame', director: 'Anthony and Joe Russo' },
-    { title: 'Culpa Mia', director: 'Domingo González' },
-    { title: 'Hitman' , director: 'Xavier Gens'}
-];
+mongoose.connect('mongodb://localhost:27017/movieAPI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Morgan middleware to log all requests to the terminal
 app.use(morgan('combined'));
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// GET route for "/movies" that returns top 10 movies in JSON format
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
+app.get('/', (req, res) => {
+    res.send('Welcome to my app!');
 });
 
-// GET route for "/" that returns a default textual response
-app.get('/', (req, res) => {
-    res.send('Welcome to my site! Go to /documentation.html to view the documentation.');
+//Get all movies
+app.get('/movies', async (req, res) => {
+    await Movies.find()
+        .then((movies) => {
+            res.json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error:  ' + err);
+        });
+});
+
+//Get a movie by title
+app.get('/movies/:Title', async (req, res) => {
+    await Movies.findOne({ Title: req.params.Title })
+        .then((movie) => {
+            if (movie) {
+                res.json(movie);
+            } else {
+                res.status(404).send(
+                    'Movie with the title ' +
+                        req.params.Title +
+                        ' was not found.'
+                );
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+//Get a genre by name
+app.get('/genres/:Name', async (req, res) => {
+    await Movies.findOne({ 'Genre.Name': req.params.Name })
+        .then((genre) => {
+            if (genre) {
+                res.json(genre.Genre);
+            } else {
+                res.status(404).send(
+                    'Genre with the name ' + req.params.Name + ' was not found.'
+                );
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+//Get a director by name
+app.get('/directors/:Name', async (req, res) => {
+    await Movies.findOne({ 'Director.Name': req.params.Name })
+        .then((director) => {
+            if (director) {
+                res.json(director.Director);
+            } else {
+                res.status(404).send(
+                    'Director with the name ' +
+                        req.params.Name +
+                        ' was not found.'
+                );
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+//Create a new user
+app.post('/users/register', async (req, res) => {
+    await Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+            if (user) {
+                return res
+                    .status(400)
+                    .send(req.body.Username + ' already exists;');
+            } else {
+                Users.create({
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday,
+                })
+                    .then((user) => {
+                        res.status(201).json(user);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(500).send('Error: ' + error);
+                    });
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
+});
+
+//Get all users
+app.get('/users', async (req, res) => {
+    await Users.find()
+        .then((users) => {
+            res.json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+//Get user by Username
+app.get('/users/:Username', async (req, res) => {
+    await Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            if (user) {
+                res.json(user);
+            } else {
+                res.status(404).send(
+                    'User with the username ' +
+                        req.params.Username +
+                        ' was not found.'
+                );
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+//Update user data by Username
+app.put('/users/:Username', async (req, res) => {
+    await Users.findOneAndUpdate(
+        { Username: req.params.Username },
+        {
+            $set: {
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday,
+            },
+        },
+        { new: true }
+    )
+        .then((updatedUser) => {
+            res.json(updatedUser);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+app.post('/users/:Username/favorites', async (req, res) => {
+    await Users.findOneAndUpdate(
+        { Username: req.params.Username },
+        {
+            $push: { Favorite: req.body.Favorite },
+        },
+        { new: true }
+    )
+        .then((updatedUser) => {
+            res.json(updatedUser);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+//Delete user by Username
+app.delete('/users/:Username', async (req, res) => {
+    await Users.findOneAndDelete({ Username: req.params.Username })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found.');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 // Error-handling middleware
---
+++ models.js
@@ -0,0 +1,40 @@
+// models.js
+const mongoose = require('mongoose');
+
+// Movie Schema
+let movieSchema = mongoose.Schema({
+    Title: { type: String, required: true},
+    Description: { type: String, required: true},
+    Director: {
+        Name: String,
+        Bio: String,
+        Birthdate: Date,
+        Deathdate: Date,
+        Movies: [String]
+    },
+     Genre: {
+        Name: String,
+        Description: String
+    },
+//     Actors: [String],
+//     ReleaseYeat: Number,
+//     ImagePath: String,
+//     Featured: Boolean
+});
+
+// User Schema
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true, unique: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true, unique: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+// Creation of the Models
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+// Export the models
+module.exports.Movie = Movie;
+module.exports.User = User;

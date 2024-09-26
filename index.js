915e4709ddca4ae63d4e5751b6b9b8557d6b93ab
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-09-26 17:45:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('xxx', {xxx});
-// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb://localhost:27017/myDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express');
 const morgan = require('morgan');
